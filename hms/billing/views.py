d1beb616670dcf4bb2c32a825219f553b3115da4
Palikaghai <palikaghai030@gmail.com>
2026-02-15 22:47:20 +0530

    commit

--- hms/billing/views.py
+++ hms/billing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import *
 
 # Create your views here.
+def billing(request):
+    return render(request,'billing/billing.html')
+
+def billing_history(request):
+    return render(request,'billing/billing_history.html')   
+
+
