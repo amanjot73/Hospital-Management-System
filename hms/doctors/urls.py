efbe18e8f01cbb588799eb286ef6f43a4a114b6e
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-02-15 17:06:10 +0530

    made login/register for doctor module

--- hms/doctors/urls.py
+++ hms/doctors/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     # path('patient_login/',views.patient_login,name = 'patient_login'),
     # path('patient_register/',views.patient_register,name = 'patient_register'),
     # path('temp/',views.temp,name = 'temp'),
+    path('pre_doctor/', views.pre_doctor, name='pre_doctor'), 
+    path('login_doctor/', views.doctor_login, name='login_doctor'),      # Remove 'accounts/'
+    path('register_doctor/', views.doctor_register, name='register_doctor'),  # Remove 'accounts/'
+    
 
 ]
