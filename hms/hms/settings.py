94cd42f8c68abed18223f7cdc7921165ea34a04e
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-02-16 13:04:58 +0530

    Updated the apps

--- hms/hms/settings.py
+++ hms/hms/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,17 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'accounts',
     'doctors',
     'patients',
-=======
     'appointments',
     'laboratory',
     'Nurse',
     'Reception',
     'pharmacy',
->>>>>>> bc332647ca32a1334991bf0e1e0103ca0f1e0e2a
 ]
 
 MIDDLEWARE = [
@@ -65,7 +62,7 @@ ROOT_URLCONF = 'hms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS':  [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,3 +135,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
