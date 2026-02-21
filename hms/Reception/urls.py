e7d0d8bc97c76d5e798817bf3d3e9d8aab15291b
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-02-21 19:18:53 +0530

    Updated the Reception part

--- hms/Reception/urls.py
+++ hms/Reception/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('pre_receptionist/', views.pre_receptionist, name='pre_receptionist'),      # Remove 'accounts/'
     path('login_receptionist/', views.receptionist_login, name='login_receptionist'),      # Remove 'accounts/'
     path('register_receptionist/', views.receptionist_register, name='register_receptionist'),  # Remove 'accounts/'
+    path('all_patient/', views.all_patient, name='all_patient'),  # Remove 'accounts/'
+    path('all_doctor/', views.all_doctor, name='all_doctor'),  # Remove 'accounts/'
 
 ]
